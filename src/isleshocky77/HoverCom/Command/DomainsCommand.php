71abca88c412fc5bb27c3bc7486ae61b3b023d6b
Stephen Ostrow <stephen@ostrow.tech>
2019-10-25 22:14:11 -0400

    add ability to filter domains by name

--- src/isleshocky77/HoverCom/Command/DomainsCommand.php
+++ src/isleshocky77/HoverCom/Command/DomainsCommand.php
@@ -7,11 +7,17 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DomainsCommand extends Command
 {
     protected static $defaultName = 'hover-com:domains:list';
+
+    public function configure()
+    {
+        $this->addOption('filter-name', null, InputOption::VALUE_OPTIONAL, 'Name to filter and show');
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
@@ -24,6 +30,11 @@ class DomainsCommand extends Command
         $table->setHeaders(['ID', 'Domain Name', 'Status', 'Renewal Date', 'Auto Renew']);
 
         foreach ($domains as $domain) {
+
+            $filterName = $input->getOption('filter-name');
+            if ($filterName && stristr($domain['domain_name'], $filterName) === false) {
+                continue;
+            }
             $table->addRow([
                 $domain['id'], $domain['domain_name'], $domain['status'], $domain['renewal_date'], $domain['auto_renew']
             ]);
