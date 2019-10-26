8b90735800076b0405e4887a959beb475e5c8b82
Stephen Ostrow <stephen@ostrow.tech>
2019-10-25 21:24:00 -0400

    Add ability to filter dns entries

--- src/isleshocky77/HoverCom/Command/DnsCommand.php
+++ src/isleshocky77/HoverCom/Command/DnsCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Helper\TableCell;
 use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DnsCommand extends Command
@@ -18,6 +19,9 @@ class DnsCommand extends Command
     protected function configure()
     {
         $this->addArgument('domains', InputArgument::IS_ARRAY, 'The domain(s) to list DNS for');
+        $this->addOption('filter-type', null, InputOption::VALUE_OPTIONAL, 'The types of records to filter and show');
+        $this->addOption('filter-name', null, InputOption::VALUE_OPTIONAL, 'The record names to filter and show');
+        $this->addOption('filter-content', null, InputOption::VALUE_OPTIONAL, 'The record content to filter and show');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -35,6 +39,22 @@ class DnsCommand extends Command
             $table->addRow([new TableCell(sprintf('<info>%s</info>', $dnss['domain_name']), ['colspan' => 6]),]);
 
             foreach ($dnss['entries'] as $dns) {
+
+                $filterType = $input->getOption('filter-type');
+                if($filterType && $dns['type'] != $filterType) {
+                    continue;
+                }
+
+                $filterName = $input->getOption('filter-name');
+                if($filterName && $dns['name'] != $filterName) {
+                    continue;
+                }
+
+                $filterContent = $input->getOption('filter-content');
+                if($filterContent && $dns['content'] != $filterContent) {
+                    continue;
+                }
+
                 $table->addRow([
                     $dns['id'], $dns['name'], $dns['type'], $dns['content'], $dns['ttl'], $dns['is_default'],
                 ]);
